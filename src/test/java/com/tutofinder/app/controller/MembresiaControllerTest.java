7be349aef7b4365f780deb99161ebf58bc32c23e
Flavio Saavedra <f.saavedra1810@gmail.com>
2021-04-29 17:35:27 -0500

    Created TutoriaController Test and Trying Membresia Controller Test

--- src/test/java/com/tutofinder/app/controller/MembresiaControllerTest.java
+++ src/test/java/com/tutofinder/app/controller/MembresiaControllerTest.java
@@ -1,4 +1,98 @@
 package com.tutofinder.app.controller;
 
+import com.tutofinder.app.dto.AlumnoDto;
+import com.tutofinder.app.dto.MembresiaDto;
+import com.tutofinder.app.dto.MembresiaDto;
+import com.tutofinder.app.dto.TutoriaDto;
+import com.tutofinder.app.dto.create.CreateMembresiaDto;
+import com.tutofinder.app.dto.create.CreateTutoriaDto;
+import com.tutofinder.app.exception.BookingException;
+import com.tutofinder.app.response.BookingResponse;
+import com.tutofinder.app.services.MembresiaService;
+import io.cucumber.java.Before;
+import io.cucumber.java.sl.In;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
 public class MembresiaControllerTest {
+    private static final String SUCCES_STATUS = "Success";
+    private static final String SUCCES_CODE = "200 OK";
+    private static final String OK = "OK";
+    private static final String MEMBRESIA_DELETED = "MEMBRESIA_DELETED";
+
+    private static final Long MEMBRESIA_ID = 1L;
+    private static final Long DOCENTE_ID_MEMBRESIA = 1L;
+    private static final Long TARJETA_ID_MEMBRESIA = 1L;
+    private static final Date FECHA_EXPIRACION_MEMBRESIA = new GregorianCalendar(2022,02,01).getTime();
+    private static final String DESCRIPCION_MEMBRESIA = "Descripcion base";
+    private static final double COSTO_MEMBRESIA = 300.0;
+
+    CreateMembresiaDto CREATE_MEMBRESIA_DTO = new CreateMembresiaDto();
+    private static final MembresiaDto MEMBRESIA_DTO = new MembresiaDto();
+    public static final List<MembresiaDto> MEMBRESIA_DTO_LIST = new ArrayList<>();
+
+    @Mock
+    MembresiaService membresiaService;
+
+    @InjectMocks
+    MembresiaController membresiaController;
+
+
+    @Before
+    public void init() throws BookingException {
+        MockitoAnnotations.initMocks(this);
+        CREATE_MEMBRESIA_DTO.setDocenteId(DOCENTE_ID_MEMBRESIA);
+        CREATE_MEMBRESIA_DTO.setTarjetaId(TARJETA_ID_MEMBRESIA);
+        CREATE_MEMBRESIA_DTO.setFechaExpiracion(FECHA_EXPIRACION_MEMBRESIA);
+        CREATE_MEMBRESIA_DTO.setDescripcionMembresia(DESCRIPCION_MEMBRESIA);
+        CREATE_MEMBRESIA_DTO.setCostoMembresia(COSTO_MEMBRESIA);
+
+        Mockito.when(membresiaService.getMembresiaById(MEMBRESIA_ID)).thenReturn(MEMBRESIA_DTO);
+        Mockito.when(membresiaService.createMembresia(CREATE_MEMBRESIA_DTO)).thenReturn(MEMBRESIA_DTO);
+        Mockito.when(membresiaService.deleteMembresia(MEMBRESIA_ID)).thenReturn(MEMBRESIA_DELETED);
+    }
+
+    @Test
+    public void createMembresiaTest() throws BookingException{
+        BookingResponse<MembresiaDto> response = membresiaController.createMembresia(CREATE_MEMBRESIA_DTO);
+        assertEquals(response.getStatus(), SUCCES_STATUS);
+        assertEquals(response.getCode(), SUCCES_CODE);
+        assertEquals(response.getMessage(), OK);
+        assertEquals(response.getData(), MEMBRESIA_DTO);
+    }
+
+    @Test
+    public void getMembresiaByIdTest() throws BookingException {
+        final BookingResponse<MembresiaDto> response = membresiaController.getMembresiaById(MEMBRESIA_ID);
+        assertEquals(response.getStatus(), SUCCES_STATUS);
+        assertEquals(response.getCode(), SUCCES_CODE);
+        assertEquals(response.getMessage(), OK);
+        assertEquals(response.getData(), MEMBRESIA_DTO);
+    }
+
+    @Test
+    public void getMembresiasTest() throws BookingException {
+        final BookingResponse<List<MembresiaDto>> response = membresiaController.getMembresias();
+        assertEquals(response.getStatus(), SUCCES_STATUS);
+        assertEquals(response.getCode(), SUCCES_CODE);
+        assertEquals(response.getMessage(), OK);
+        assertEquals(response.getData(), MEMBRESIA_DTO_LIST);
+    }
+
+    @Test
+    public void deleteMembresiaTest() throws BookingException{
+        final BookingResponse<String> response = membresiaController.deleteMembresia(MEMBRESIA_ID);
+        assertEquals(response.getStatus(), SUCCES_STATUS);
+        assertEquals(response.getCode(), SUCCES_CODE);
+        assertEquals(response.getMessage(), OK);
+        assertEquals(response.getData(), MEMBRESIA_DELETED);
+    }
 }
--- src/test/java/com/tutofinder/app/controller/TutoriaControllerTest.java
+++ src/test/java/com/tutofinder/app/controller/TutoriaControllerTest.java
@@ -1,4 +1,97 @@
 package com.tutofinder.app.controller;
 
+import com.tutofinder.app.dto.TutoriaDto;
+import com.tutofinder.app.dto.create.CreateTutoriaDto;
+import com.tutofinder.app.exception.BookingException;
+import com.tutofinder.app.response.BookingResponse;
+import com.tutofinder.app.services.TutoriaService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class TutoriaControllerTest {
+    private static final String SUCCES_STATUS = "Success";
+    private static final String SUCCES_CODE = "200 OK";
+    private static final String OK = "OK";
+
+    private static final Long TUTORIA_ID = 1L;
+    private static final int CANTIDAD_HORAS_TUTORIA = 2;
+    private static final String DESCRIPCION_TUTORIA = "Descripcion base";
+    private static final Long CURSO_ID_TUTORIA = 1L;
+    private static final Long DOCENTE_ID_TUTORIA = 1L;
+
+    CreateTutoriaDto CREATE_TUTORIA_DTO = new CreateTutoriaDto();
+    private static final String TUTORIA_DELETED = "TUTORIA_DELETED";
+    private static final TutoriaDto TUTORIA_DTO = new TutoriaDto();
+    public static final List<TutoriaDto> TUTORIA_DTO_LIST = new ArrayList<>();
+
+    @Mock
+    TutoriaService tutoriaService;
+
+    @InjectMocks
+    TutoriaController tutoriaController;
+
+    @Before
+    public void init() throws BookingException {
+        MockitoAnnotations.initMocks(this);
+        CREATE_TUTORIA_DTO.setDescripcionTutoria(DESCRIPCION_TUTORIA);
+        CREATE_TUTORIA_DTO.setCantidadHoras(CANTIDAD_HORAS_TUTORIA);
+        CREATE_TUTORIA_DTO.setDocenteId(DOCENTE_ID_TUTORIA);
+        CREATE_TUTORIA_DTO.setCursoId(CURSO_ID_TUTORIA);
+
+        Mockito.when(tutoriaService.getTutoriaById(TUTORIA_ID)).thenReturn(TUTORIA_DTO);
+        Mockito.when(tutoriaService.createTutoria(CREATE_TUTORIA_DTO)).thenReturn(TUTORIA_DTO);
+        Mockito.when(tutoriaService.deleteTutoria(TUTORIA_ID)).thenReturn(TUTORIA_DELETED);
+    }
+
+    @Test
+    public void createTutoriaTest() throws BookingException{
+        BookingResponse<TutoriaDto> response = tutoriaController.createTutoria(CREATE_TUTORIA_DTO);
+        assertEquals(response.getStatus(), SUCCES_STATUS);
+        assertEquals(response.getCode(), SUCCES_CODE);
+        assertEquals(response.getMessage(), OK);
+        assertEquals(response.getData(), TUTORIA_DTO);
+    }
+
+    @Test
+    public void getTutoriaByIdTest() throws BookingException {
+        final BookingResponse<TutoriaDto> response = tutoriaController.getTutoriaById(TUTORIA_ID);
+
+        assertEquals(response.getStatus(), SUCCES_STATUS);
+        assertEquals(response.getCode(), SUCCES_CODE);
+        assertEquals(response.getMessage(), OK);
+        assertEquals(response.getData(), TUTORIA_DTO);
+
+    }
+
+    @Test
+    public void getTutoriasTest() throws BookingException {
+        final BookingResponse<List<TutoriaDto>> response = tutoriaController.getTutorias();
+
+        assertEquals(response.getStatus(), SUCCES_STATUS);
+        assertEquals(response.getCode(), SUCCES_CODE);
+        assertEquals(response.getMessage(), OK);
+        assertEquals(response.getData(), TUTORIA_DTO_LIST);
+
+    }
+
+    @Test
+    public void deleteTutoriaTest() throws BookingException{
+
+        final BookingResponse<String> response = tutoriaController.deleteTutoria(TUTORIA_ID);
+
+        assertEquals(response.getStatus(), SUCCES_STATUS);
+        assertEquals(response.getCode(), SUCCES_CODE);
+        assertEquals(response.getMessage(), OK);
+        assertEquals(response.getData(), TUTORIA_DELETED);
+
+    }
 }
