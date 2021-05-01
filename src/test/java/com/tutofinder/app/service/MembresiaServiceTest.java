df574af766ecf0bbedaf0ee0221990357ad43072
William <wmavalle@gmail.com>
2021-04-30 21:46:23 -0500

    membresiatest

--- src/test/java/com/tutofinder/app/service/MembresiaServiceTest.java
+++ src/test/java/com/tutofinder/app/service/MembresiaServiceTest.java
@@ -1,18 +1,28 @@
 package com.tutofinder.app.service;
 
-public class MembresiaServiceTest {
-    private static final Long INFORME_ID = 1L;
-    private static final String DESCRIPCION_INFORME = "bueno";
-    private static final Long TUTORIA_ID = 1L;
-    private static final Long ALUMNO_ID = 1L;
-    public static final Informe INFORME = new Informe();
+import com.tutofinder.app.dto.create.CreateMembresiaDto;
+import com.tutofinder.app.entity.Docente;
+import com.tutofinder.app.entity.Membresia;
+import com.tutofinder.app.entity.Tarjeta;
 
-    CreateInformeDto CREATE_INFORME_DTO = new CreateInformeDto();
+import java.util.Date;
+import java.util.Optional;
+
+import static org.assertj.core.util.DateUtil.now;
+
+public class MembresiaServiceTest {
+    private static final Long MEMBRESIA_ID = 1L;
+    private static final String DESCRIPCION_MEMBRESIA = "bueno";
+    private static final Double COSTO_MEMBRESIA = 45.8;
+    private static final Date FECHA_EXPIRACION = now();
+    public static final Membresia MEMBRESIA = new Membresia();
+
+    CreateMembresiaDto CREATE_MEMBRESIA_DTO = new CreateMembresiaDto();
     private static final String INFORME_DELETED = "INFORME_DELETED";
-    private static final Optional<Informe> OPTIONAL_INFORME_EMPTY = Optional.empty();
-    private static final Optional<Informe> OPTIONAL_INFORME = Optional.of(INFORME);
-    private static final Optional<Tutoria> OPTIONAL_TUTORIA = Optional.of(new Tutoria());
-    private static final Optional<Alumno> OPTIONAL_ALUMNO = Optional.of(new Alumno());
+    private static final Optional<Membresia> OPTIONAL_INFORME_EMPTY = Optional.empty();
+    private static final Optional<Membresia> OPTIONAL_INFORME = Optional.of(MEMBRESIA);
+    private static final Optional<Tarjeta> OPTIONAL_TUTORIA = Optional.of(new Tarjeta());
+    private static final Optional<Docente> OPTIONAL_ALUMNO = Optional.of(new Docente());
 
 
 
