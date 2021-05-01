abdda62fd5e840a6b7153b017f46f06c7d7b5dc9
William <wmavalle@gmail.com>
2021-04-30 22:24:35 -0500

    membresia-finally

--- src/test/java/com/tutofinder/app/service/MembresiaServiceTest.java
+++ src/test/java/com/tutofinder/app/service/MembresiaServiceTest.java
@@ -1,120 +1,137 @@
 package com.tutofinder.app.service;
 
+import com.tutofinder.app.dto.MembresiaDto;
 import com.tutofinder.app.dto.create.CreateMembresiaDto;
 import com.tutofinder.app.entity.Docente;
 import com.tutofinder.app.entity.Membresia;
 import com.tutofinder.app.entity.Tarjeta;
+import com.tutofinder.app.exception.BookingException;
 import com.tutofinder.app.repository.AlumnoRepository;
+import com.tutofinder.app.repository.DocenteRepository;
 import com.tutofinder.app.repository.MembresiaRepository;
 import com.tutofinder.app.repository.TarjetaRepository;
 import com.tutofinder.app.services.impl.MembresiaServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.exceptions.base.MockitoException;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.util.DateUtil.now;
+import static org.junit.Assert.*;
 
 public class MembresiaServiceTest {
     private static final Long MEMBRESIA_ID = 1L;
     private static final String DESCRIPCION_MEMBRESIA = "bueno";
     private static final Double COSTO_MEMBRESIA = 45.8;
     private static final Date FECHA_EXPIRACION = now();
+    private static final Long TARJETA_ID = 1L;
+    private static final Long DOCENTE_ID = 1L;
     public static final Membresia MEMBRESIA = new Membresia();
 
+
     CreateMembresiaDto CREATE_MEMBRESIA_DTO = new CreateMembresiaDto();
-    private static final String INFORME_DELETED = "INFORME_DELETED";
+    private static final String MEMBRESIA_DELETED = "INFORME_DELETED";
     private static final Optional<Membresia> OPTIONAL_INFORME_EMPTY = Optional.empty();
-    private static final Optional<Membresia> OPTIONAL_INFORME = Optional.of(MEMBRESIA);
-    private static final Optional<Tarjeta> OPTIONAL_TUTORIA = Optional.of(new Tarjeta());
-    private static final Optional<Docente> OPTIONAL_ALUMNO = Optional.of(new Docente());
+    private static final Optional<Membresia> OPTIONAL_MEMBRESIA = Optional.of(MEMBRESIA);
+    private static final Optional<Tarjeta> OPTIONAL_TARJETA = Optional.of(new Tarjeta());
+    private static final Optional<Docente> OPTIONAL_DOCENTE = Optional.of(new Docente());
 
     @Mock
-    MembresiaRepository informeRepository;
+    MembresiaRepository membresiaRepository;
 
     @Mock
-    TarjetaRepository tutoriaRepository;
+    TarjetaRepository tarjetaRepository;
 
     @Mock
-    AlumnoRepository alumnoRepository;
+    DocenteRepository docenteRepository;
 
     @InjectMocks
-    MembresiaServiceImpl informeServiceImpl;
+    MembresiaServiceImpl membresiaServiceImpl;
 
     @Before
     public void init() throws BookingException {
         MockitoAnnotations.initMocks(this);
 
-        INFORME.setId(INFORME_ID);
-        INFORME.setDescripcionInforme(DESCRIPCION_INFORME);
+        MEMBRESIA.setId(MEMBRESIA_ID);
+        MEMBRESIA.setDescripcionMembresia(DESCRIPCION_MEMBRESIA);
 
-        CREATE_INFORME_DTO.setDescripcionInforme(DESCRIPCION_INFORME);
-        CREATE_INFORME_DTO.setTutoriaId(TUTORIA_ID);
-        CREATE_INFORME_DTO.setAlumnoId(ALUMNO_ID);
+        CREATE_MEMBRESIA_DTO.setDescripcionMembresia(DESCRIPCION_MEMBRESIA);
+        CREATE_MEMBRESIA_DTO.setCostoMembresia(COSTO_MEMBRESIA);
+        CREATE_MEMBRESIA_DTO.setFechaExpiracion(FECHA_EXPIRACION);
+        CREATE_MEMBRESIA_DTO.setTarjetaId(TARJETA_ID);
+        CREATE_MEMBRESIA_DTO.setDocenteId(DOCENTE_ID);
+
     }
 
     @Test
-    public void getInformeByIdTest() throws BookingException{
-        Mockito.when(informeRepository.findById(INFORME_ID)).thenReturn(OPTIONAL_INFORME);
-        informeServiceImpl.getInformeById(INFORME_ID);
+    public void getMembresiaByIdTest() throws BookingException{
+        Mockito.when(membresiaRepository.findById(MEMBRESIA_ID)).thenReturn(OPTIONAL_MEMBRESIA);
+        membresiaServiceImpl.getMembresiaById(MEMBRESIA_ID);
     }
 
     @Test
-    public void getInformesTest() throws BookingException{
-        final Informe informe = new Informe();
-        Mockito.when(informeRepository.findAll()).thenReturn(Arrays.asList(informe));
-        final List<InformeDto> response = informeServiceImpl.getInformes();
+    public void getMembresiaTest() throws BookingException{
+        final Membresia membresia = new Membresia();
+        Mockito.when(membresiaRepository.findAll()).thenReturn(Arrays.asList(membresia));
+        final List<MembresiaDto> response = membresiaServiceImpl.getMembresias();
         assertNotNull(response);
         assertFalse(response.isEmpty());
         assertEquals(response.size(), 1);
     }
 
     @Test(expected = BookingException.class)
-    public void getInformeByIdTestError() throws BookingException{
-        Mockito.when(informeRepository.findById(INFORME_ID)).thenReturn(OPTIONAL_INFORME_EMPTY);
-        informeServiceImpl.getInformeById(INFORME_ID);
+    public void getMembresiaByIdTestError() throws BookingException{
+        Mockito.when(membresiaRepository.findById(MEMBRESIA_ID)).thenReturn(OPTIONAL_INFORME_EMPTY);
+        membresiaServiceImpl.getMembresiaById(MEMBRESIA_ID);
         fail();
     }
 
     @Test
-    public void createInformeTest() throws BookingException{
-        Mockito.when(tutoriaRepository.findById(TUTORIA_ID)).thenReturn(OPTIONAL_TUTORIA);
-        Mockito.when(alumnoRepository.findById(ALUMNO_ID)).thenReturn(OPTIONAL_ALUMNO);
-        Mockito.when(informeRepository.findById(INFORME_ID)).thenReturn(OPTIONAL_INFORME);
-        Mockito.when(informeRepository.save(Mockito.any(Informe.class))).thenReturn(INFORME);
-        informeServiceImpl.createInforme(CREATE_INFORME_DTO);
+    public void createMembresiaTest() throws BookingException{
+        Mockito.when(tarjetaRepository.findById(TARJETA_ID)).thenReturn(OPTIONAL_TARJETA);
+        Mockito.when(docenteRepository.findById(DOCENTE_ID)).thenReturn(OPTIONAL_DOCENTE);
+        Mockito.when(membresiaRepository.findById(MEMBRESIA_ID)).thenReturn(OPTIONAL_MEMBRESIA);
+        Mockito.when(membresiaRepository.save(Mockito.any(Membresia.class))).thenReturn(MEMBRESIA);
+        membresiaServiceImpl.createMembresia(CREATE_MEMBRESIA_DTO);
     }
 
     @Test(expected = MockitoException.class)
-    public void createInformeInternalServerErrorTest() throws BookingException {
-        Mockito.when(informeRepository.findById(INFORME_ID)).thenReturn(OPTIONAL_INFORME);
-        Mockito.doThrow(Exception.class).when(informeRepository).save(Mockito.any(Informe.class));
-        informeServiceImpl.createInforme(CREATE_INFORME_DTO);
+    public void createMembresiaInternalServerErrorTest() throws BookingException {
+        Mockito.when(membresiaRepository.findById(MEMBRESIA_ID)).thenReturn(OPTIONAL_MEMBRESIA);
+        Mockito.doThrow(Exception.class).when(membresiaRepository).save(Mockito.any(Membresia.class));
+        membresiaServiceImpl.createMembresia(CREATE_MEMBRESIA_DTO);
         fail();
     }
 
     @Test
-    public void deleteInformeOk() throws BookingException {
-        Mockito.when(informeRepository.findById(INFORME_ID)).thenReturn(OPTIONAL_INFORME);
-        final String response = informeServiceImpl.deleteInforme(INFORME_ID);
-        assertEquals(response, INFORME_DELETED);
+    public void deleteMembresiaOk() throws BookingException {
+        Mockito.when(membresiaRepository.findById(MEMBRESIA_ID)).thenReturn(OPTIONAL_MEMBRESIA);
+        final String response = membresiaServiceImpl.deleteMembresia(MEMBRESIA_ID);
+        assertEquals(response, MEMBRESIA_DELETED);
     }
 
     @Test(expected = BookingException.class)
-    public void deleteInformeNotFountError() throws BookingException {
-        Mockito.when(informeRepository.findById(INFORME_ID)).thenReturn(OPTIONAL_INFORME_EMPTY);
-        final String response = informeServiceImpl.deleteInforme(INFORME_ID);
-        assertEquals(response, INFORME_DELETED);
+    public void deleteMembresiaNotFountError() throws BookingException {
+        Mockito.when(membresiaRepository.findById(MEMBRESIA_ID)).thenReturn(OPTIONAL_INFORME_EMPTY);
+        final String response = membresiaServiceImpl.deleteMembresia(MEMBRESIA_ID);
+        assertEquals(response, MEMBRESIA_DELETED);
         fail();
     }
 
     @Test(expected = MockitoException.class)
-    public void deleteInformeInternalServerError() throws BookingException {
-        Mockito.when(informeRepository.findById(INFORME_ID)).thenReturn(OPTIONAL_INFORME);
-        Mockito.doThrow(Exception.class).when(informeRepository).deleteById(INFORME_ID);
-        final String response = informeServiceImpl.deleteInforme(INFORME_ID);
-        assertEquals(response, INFORME_DELETED);
+    public void deleteMembresiaInternalServerError() throws BookingException {
+        Mockito.when(membresiaRepository.findById(MEMBRESIA_ID)).thenReturn(OPTIONAL_MEMBRESIA);
+        Mockito.doThrow(Exception.class).when(membresiaRepository).deleteById(MEMBRESIA_ID);
+        final String response = membresiaServiceImpl.deleteMembresia(MEMBRESIA_ID);
+        assertEquals(response, MEMBRESIA_DELETED);
         fail();
     }
 
