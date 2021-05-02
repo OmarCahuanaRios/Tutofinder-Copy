b450131ba3d541052a71152d62f01c21a76756fc
Moises Molina <w7moises@gmail.com>
2021-05-02 12:21:20 -0500

    'query-for-tarjeta-added'

--- src/main/java/com/tutofinder/app/repository/TarjetaRepository.java
+++ src/main/java/com/tutofinder/app/repository/TarjetaRepository.java
@@ -2,11 +2,19 @@ package com.tutofinder.app.repository;
 
 import com.tutofinder.app.entity.Tarjeta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TarjetaRepository extends JpaRepository<Tarjeta,Long> {
     Optional<Tarjeta> findById(Long id);
+
+    Optional<Tarjeta> findByNombrePoseedorAndNumeroTarjeta(String nombre,String numero);
+
+    @Query("select t from Tarjeta t where upper(t.nombrePoseedor) like upper(concat('%',?1,'%'))")
+    List<Tarjeta> findByNombrePoseedor(String nombre);
+
 }
