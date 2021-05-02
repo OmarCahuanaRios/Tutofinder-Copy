09867d226c5f4d82b37929c44b588295fa514a30
Moises Molina <w7moises@gmail.com>
2021-05-02 13:59:54 -0500

    'curso-repository-query'

--- src/main/java/com/tutofinder/app/repository/CursoRepository.java
+++ src/main/java/com/tutofinder/app/repository/CursoRepository.java
@@ -2,6 +2,7 @@ package com.tutofinder.app.repository;
 
 import com.tutofinder.app.entity.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +12,6 @@ public interface CursoRepository extends JpaRepository<Curso,Long> {
 
     Optional<Curso> findById(Long id);
 
+    @Query("select c from Curso c where upper(c.nombre) like upper(concat('%',?1,'%'))")
+    Optional<Curso> findByNombre(String nombre);
 }
